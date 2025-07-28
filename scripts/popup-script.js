5cd2502c4c5cd6195ea06a4d42263bf9a6e7036a
endeav007 <autumnaustin046@gmail.com>
2025-07-28 16:01:48 -0700

    Finalized Resume Tab

---
+++ scripts/popup-script.js
@@ -0,0 +1,6 @@
+function closePopup(){
+    /*alert('Clicked!');*/
+    document.getElementById("popup").style.borderColor = "green";
+    document.getElementById("popup").style.display = "none";
+
+}
